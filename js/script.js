99d129bda29b5739a973a4221b342b016df4dad5
why-edan <im.vedantsharma@gmail.com>
2026-09-15 12:04:56 +0530

    css

--- js/script.js
+++ js/script.js
@@ -444,6 +444,29 @@ function renderSchedule() {
       <button class="delete-exercise" data-delete-exercise="${exercise.id}" title="Remove exercise">×</button>
     </div>
   `).join("");
+
+  document.querySelectorAll(".ex-input.name-input").forEach(fitNameInput);
+}
+
+/* Shrinks a name input's font-size (and padding) just enough for the
+   full text to fit on one line, instead of letting it truncate. */
+function fitNameInput(input) {
+  const max = 16;
+  const min = 9.5;
+  input.style.letterSpacing = "normal";
+  input.style.paddingLeft = "";
+  input.style.paddingRight = "";
+  input.style.fontSize = max + "px";
+  let size = max;
+  while (input.scrollWidth > input.clientWidth && size > min) {
+    size -= 0.5;
+    input.style.fontSize = size + "px";
+  }
+  if (input.scrollWidth > input.clientWidth) {
+    input.style.paddingLeft = "5px";
+    input.style.paddingRight = "5px";
+    input.style.letterSpacing = "-0.3px";
+  }
 }
 
 dayTabsEl.addEventListener("click", e => {
@@ -470,9 +493,14 @@ scheduleList.addEventListener("input", e => {
 
   if (input.classList.contains("name-input")) {
     exercise.name = input.value;
+    fitNameInput(input);
   } else {
     exercise.sets = input.value;
   }
+});
+
+window.addEventListener("resize", () => {
+  document.querySelectorAll(".ex-input.name-input").forEach(fitNameInput);
 });
 
 scheduleList.addEventListener("change", () => {
